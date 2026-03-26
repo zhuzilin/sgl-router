0de1f7de7e4d845692e940f9886b4d5f8b3d1b19
Zilin Zhu <zilin.zhu@aminer.cn>
2026-03-26 01:11:03 +0000

    bugfix

--- bindings/python/src/sglang_router/version.py
+++ bindings/python/src/sglang_router/version.py
@@ -1,7 +1,7 @@
 _base_version = "0.3.2"
 
 try:
-    from sglang_router_rs import get_git_commit
+    from sglang_router.sglang_router_rs import get_git_commit
     _commit = get_git_commit()
     __version__ = f"{_base_version}-{_commit}" if _commit and _commit != "unknown" else _base_version
 except ImportError:
